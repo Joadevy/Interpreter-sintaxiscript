205046f753d3047d396653a021cb819d95e74fc0
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-06-01 00:48:28 -0300

    Modulo para manejo del input file refactorizado

--- src/typescript/main/index.ts
+++ src/typescript/main/index.ts
@@ -1,8 +1,11 @@
-import {mostrarArchivo} from '../Analizador Lexico/index.js';
+import {interprete} from './interprete.js';
 
-// Muestra el contenido del archivo en el HTML, es la input del analizador lexico.
+// Este modulo tiene como responsabilidad el manejo del input file.
 
+// Tomamos el elemento HTML del input file.
 const input = document.getElementById('input');
+
+// Establecemos las acciones inmediatas luego de que se sube un archivo.
 input?.addEventListener('change', (e) => {
     const archivos = (e.target as HTMLInputElement).files;
     // Si se subio al menos un archivo.
@@ -11,7 +14,8 @@ input?.addEventListener('change', (e) => {
       let compruebaTXT = /\w+\.txt$/;
       // Usamos solamente el primer archivo que se haya pasado (aunque por defecto solo se puede pasar uno)
       if (compruebaTXT.test(archivos[0].name)){
-        mostrarArchivo(archivos[0]);
+        // Llamamos a la funcion que se encargara del manejo del compilador.
+        interprete(archivos[0]);
       } else {
         alert('Porfavor, introduce un archivo de texto .TXT')
       }
