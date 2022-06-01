56583860a0aec8ba6e270bd817297bfd27dc505e
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-05-31 22:23:06 -0300

    Arreglada la comprobacion del e.target.files con posible null

--- build/main/index.js
+++ build/main/index.js
@@ -11,12 +11,14 @@ import { esConstEntera } from '../Automatas/constanteEntera.js';
 // Muestra el contenido del archivo en el HTML, es la input del analizador lexico.
 const input = document.getElementById('input');
 input === null || input === void 0 ? void 0 : input.addEventListener('change', (e) => {
-    if (e.target.files[0] != null) { // No entiendo porque esto no lo soluciona, nunca puede ser null.
-        const archivo = e.target.files[0];
+    const archivos = e.target.files;
+    // Si se subio al menos un archivo.
+    if (archivos != null) {
         // Expresion regular que comprueba que se haya pasado un .txt
         let compruebaTXT = /\w+\.txt$/;
-        if (compruebaTXT.test(archivo.name)) {
-            mostrarArchivo(archivo);
+        // Usamos solamente el primer archivo que se haya pasado (aunque por defecto solo se puede pasar uno)
+        if (compruebaTXT.test(archivos[0].name)) {
+            mostrarArchivo(archivos[0]);
         }
         else {
             alert('Porfavor, introduce un archivo de texto .TXT');
--- src/typescript/main/index.ts
+++ src/typescript/main/index.ts
@@ -4,16 +4,18 @@ import {esConstEntera} from '../Automatas/constanteEntera.js';
 
 const input = document.getElementById('input');
 input?.addEventListener('change', (e) => {
-  if (e.target.files[0] != null) { // No entiendo porque esto no lo soluciona, nunca puede ser null.
-    const archivo = e.target.files[0];
-  // Expresion regular que comprueba que se haya pasado un .txt
-    let compruebaTXT = /\w+\.txt$/;
-    if (compruebaTXT.test(archivo.name)){
-      mostrarArchivo(archivo);
-    } else {
-      alert('Porfavor, introduce un archivo de texto .TXT')
+    const archivos = (e.target as HTMLInputElement).files;
+    // Si se subio al menos un archivo.
+    if (archivos != null) {
+      // Expresion regular que comprueba que se haya pasado un .txt
+      let compruebaTXT = /\w+\.txt$/;
+      // Usamos solamente el primer archivo que se haya pasado (aunque por defecto solo se puede pasar uno)
+      if (compruebaTXT.test(archivos[0].name)){
+        mostrarArchivo(archivos[0]);
+      } else {
+        alert('Porfavor, introduce un archivo de texto .TXT')
+      }
     }
-  }
 })
 
 
