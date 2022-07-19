a5e71e1712d19fde520ec6a3f5129399fe610178
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-07-18 21:26:56 -0300

    Cambios menores

--- build/Analizador Sintactico/index.js
+++ build/Analizador Sintactico/index.js
@@ -2,7 +2,7 @@ import { CrearArbol } from "./manejoArbol";
 import { nodo } from "./manejoArbol";
 export function AnalizadorSintactico(codigoFuente /*, raiz:nodo */) {
     console.log('TEST');
-    let nodoPrueba = new nodo('tProgram', 'Program', 0, []);
+    let nodoPrueba = new nodo('tPrograma', 'Program', 0, []);
     let arbol = new CrearArbol(nodoPrueba);
     let hijoPrueba = new nodo('tId', 'calculadora', 0, []);
     arbol.insertarHijo(hijoPrueba);
