d5a8d6d3e3e9b62906c26ab1d496264069173491
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-07-19 20:57:03 -0300

    Comportamiento de la pila para el A. Sintactico

---
+++ src/typescript/Analizador Sintactico/manejoPila.ts
@@ -0,0 +1,39 @@
+import {simboloGramatical,Arbol,nodo} from './manejoArbol';
+
+/* La pila contendra a pesos y al simbolo inicial, en ese orden.
+El simbolo inicial tendra el arbol donde a medida que se leen los compLex del analizador lexico se van agregando al arbol y
+esta es la salida del analizador lexico. */
+
+/* A partir de la TAS se obtienen los simbolos gramaticales (variables/terminales) que se tienen que ir encontrando en el
+analizador lexico para que sa correcto sintacticamente. Esos se apilan de a uno en la pila para ir repitiendo el proceso.
+*/
+
+type elementoPila = {
+    simbolo: simboloGramatical;
+    arbolPila?: nodo
+}
+
+export function crearPila():Array<elementoPila>{ 
+    return []
+}
+
+export function Apilar(pila:Array<elementoPila>, elemento:elementoPila){
+    pila.push(elemento);
+}
+
+export function Desapilar(pila:Array<elementoPila>):elementoPila|undefined{
+    return pila.pop();
+}
+
+// TESTEANDO FUNCIONES
+let simboloInicial = new nodo('tPrograma','PROGRAM',0,[]);
+let nodo1 = new nodo('tId','CALCULADORA',0,[]);
+let arbol = new Arbol(simboloInicial);
+arbol.insertarHijo(nodo1);
+let pila = crearPila();
+Apilar(pila,{simbolo:'pesos'})
+Apilar(pila, {simbolo: 'vPROGRAMA', arbolPila: arbol});
+let elemento = Desapilar(pila) // Toma el elemento de la cima de la pila (que es el que contiene el arbol)
+console.log(pila) // Ahora solo queda pesos en el arbol.
+console.log(pila.length) // devuelve 1.
+console.log(elemento) // Devuelve el objeto con el simbolo inicial y el arbol donde se ira acumulando la salida del Analizador sintactico
