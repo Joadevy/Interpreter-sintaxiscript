f3bc9d552cad59863ddeb0696c4e74e050a7eccf
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-07-21 04:29:35 -0300

    Modificados tMas y tMenos por tSuma  tResta

--- src/typescript/Analizador Sintactico/TAS.ts
+++ src/typescript/Analizador Sintactico/TAS.ts
@@ -1,11 +1,11 @@
 // export type variables = 'vPROGRAMA' | 'vCUERPO'| 'vSENTENCIAS' | 'vSENTENCIA' | 'vDECLARACION' | 'vVARIABLES' | 'vVARIABLE' | 'vASIGNACION' | 'vEXPARIT' | 'vIZQARIT' | 'vRAIZPOT' | 'vPOT' | 'vSUMARESTA' | 'vMULTDIV' | 'vOPERANDOS' | 'vLECTURA' | 'vESCRITURA' | 'vSALIDAS' | 'vSAUX' | 'vSALIDA' | 'vCONDICIONAL' | 'vCONDICIONALFACT' | 'vMIENTRAS' | 'vCONDICION' | 'vIZQCOND' | 'vNEGACION' | 'vCONJUNCION' | 'vDISYUNCION' ;
 // export type terminales = 'tPrograma' | 'tEscribir' | 'tVariables' | 'tLeer' | 'tWhile' | 'tIf' | 'tElse' | 'tAnd' | 'tOr' | 'tNot' | 
-// 'tId' | 'tCadena' | 'tConstReal' | 'tPuntoComa' | 'tComa' | 'tOpRel' | 'tOpAsignacion' | 'tMas' | 'tMenos' | 'tProducto' | 'tDivision' |'tPotencia' | 'tRaiz' | 
+// 'tId' | 'tCadena' | 'tConstReal' | 'tPuntoComa' | 'tComa' | 'tOpRel' | 'tOpAsignacion' | 'tSuma' | 'tResta' | 'tProducto' | 'tDivision' |'tPotencia' | 'tRaiz' | 
 // 'tParentesisAbre' | 'tParentesisCierra' | 'tLlaveAbre' | 'tLlaveCierra' | 'tCorcheteAbre' | 'tCorcheteCierra';
 //import { simboloGramatical } from "./manejoArbol";
 
 type simboloGramatical = 'vPROGRAMA' | 'vCUERPO'| 'vSENTENCIAS' | 'vSENTENCIA' | 'vDECLARACION' | 'vVARIABLES' | 'vVARIABLE' | 'vASIGNACION' | 'vEXPARIT' | 'vIZQARIT' | 'vRAIZPOT' | 'vPOT' | 'vSUMARESTA' | 'vMULTDIV' | 'vOPERANDOS' | 'vLECTURA' | 'vESCRITURA' | 'vSALIDAS' | 'vSAUX' | 'vSALIDA' | 'vCONDICIONAL' | 'vCONDICIONALFACT' | 'vMIENTRAS' | 'vCONDICION' | 'vIZQCOND' | 'vNEGACION' | 'vCONJUNCION' | 'vDISYUNCION' |'tPrograma' | 'tEscribir' | 'tVariables' | 'tLeer' | 'tWhile' | 'tIf' | 'tElse' | 'tAnd' | 'tOr' | 'tNot' | 
-'tId' | 'tCadena' | 'tConstReal' | 'tPuntoComa' | 'tComa' | 'tOpRel' | 'tOpAsignacion' | 'tMas' | 'tMenos' | 'tProducto' | 'tDivision' |'tPotencia' | 'tRaiz' | 
+'tId' | 'tCadena' | 'tConstReal' | 'tPuntoComa' | 'tComa' | 'tOpRel' | 'tOpAsignacion' | 'tSuma' | 'tResta' | 'tProducto' | 'tDivision' |'tPotencia' | 'tRaiz' | 
 'tParentesisAbre' | 'tParentesisCierra' | 'tLlaveAbre' | 'tLlaveCierra' | 'tCorcheteAbre' | 'tCorcheteCierra';
 
 export enum variables{
@@ -14,7 +14,7 @@ export enum variables{
 
 export enum terminales{
   'tPrograma' , 'tEscribir' , 'tVariables' , 'tLeer' , 'tWhile' , 'tIf' , 'tElse' , 'tAnd' , 'tOr' , 'tNot' , 
-  'tId' , 'tCadena' , 'tConstReal' , 'tPuntoComa' , 'tComa' , 'tOpRel' , 'tOpAsignacion' , 'tMas' , 'tMenos' , 'tProducto' , 'tDivision' ,'tPotencia' , 'tRaiz' , 
+  'tId' , 'tCadena' , 'tConstReal' , 'tPuntoComa' , 'tComa' , 'tOpRel' , 'tOpAsignacion' , 'tSuma' , 'tResta' , 'tProducto' , 'tDivision' ,'tPotencia' , 'tRaiz' , 
   'tParentesisAbre' , 'tParentesisCierra' , 'tLlaveAbre' , 'tLlaveCierra' , 'tCorcheteAbre' , 'tCorcheteCierra'
 }
 
@@ -57,8 +57,8 @@ function cargaEProduccionesTAS(TAS:Array<any>):Array<any>{
   TAS[variables.vPOT][terminales.tPuntoComa] = new celda ([]);
   TAS[variables.vPOT][terminales.tComa] = new celda ([]);
   TAS[variables.vPOT][terminales.tCorcheteCierra] = new celda ([]);
-  TAS[variables.vPOT][terminales.tMas] = new celda ([]);
-  TAS[variables.vPOT][terminales.tMenos] = new celda ([]);
+  TAS[variables.vPOT][terminales.tSuma] = new celda ([]);
+  TAS[variables.vPOT][terminales.tResta] = new celda ([]);
   TAS[variables.vPOT][terminales.tProducto] = new celda ([]);
   TAS[variables.vPOT][terminales.tDivision] = new celda ([]);
   TAS[variables.vPOT][terminales.tCorcheteCierra] = new celda ([]);
@@ -79,8 +79,8 @@ function cargaEProduccionesTAS(TAS:Array<any>):Array<any>{
   TAS[variables.vMULTDIV][terminales.tParentesisCierra] = new celda ([]);
   TAS[variables.vMULTDIV][terminales.tPuntoComa] = new celda ([]);
   TAS[variables.vMULTDIV][terminales.tComa] = new celda ([]);
-  TAS[variables.vMULTDIV][terminales.tMas] = new celda ([]);
-  TAS[variables.vMULTDIV][terminales.tMenos] = new celda ([]);
+  TAS[variables.vMULTDIV][terminales.tSuma] = new celda ([]);
+  TAS[variables.vMULTDIV][terminales.tResta] = new celda ([]);
   TAS[variables.vMULTDIV][terminales.tCorcheteCierra] = new celda ([]);
   TAS[variables.vMULTDIV][terminales.tOpRel] = new celda ([]);
   TAS[variables.vMULTDIV][terminales.tAnd] = new celda ([]);
@@ -101,7 +101,7 @@ export function cargarTAS(TAS:Array<any>):Array<any>{
     TAS = cargaEProduccionesTAS(TAS); // Primero carga todas las epsilon producciones.
     
     // Cargando cada celda de la TAS de acuerdo a la construida a partir de la CFG.
-    TAS[variables.vPROGRAMA][terminales.tPrograma] = new celda (['tPrograma','tId','tCorcheteAbre','vCUERPO','tCorcheteCierra']);
+    TAS[variables.vPROGRAMA][terminales.tPrograma] = new celda (['tPrograma','tId','tLlaveAbre','vCUERPO','tLlaveCierra']);
     
     TAS[variables.vCUERPO][terminales.tId] = new celda (['vSENTENCIA','vSENTENCIAS']);
     TAS[variables.vCUERPO][terminales.tVariables] = new celda (['vSENTENCIA','vSENTENCIAS']);
@@ -130,32 +130,32 @@ export function cargarTAS(TAS:Array<any>):Array<any>{
     TAS[variables.vEXPARIT][terminales.tId] = new celda (['vIZQARIT','vSUMARESTA']);
     TAS[variables.vEXPARIT][terminales.tParentesisAbre] = new celda (['vIZQARIT','vSUMARESTA']);
     TAS[variables.vEXPARIT][terminales.tRaiz] = new celda (['vIZQARIT','vSUMARESTA']);
-    TAS[variables.vEXPARIT][terminales.tMenos] = new celda (['vIZQARIT','vSUMARESTA']);
+    TAS[variables.vEXPARIT][terminales.tResta] = new celda (['vIZQARIT','vSUMARESTA']);
     TAS[variables.vEXPARIT][terminales.tConstReal] = new celda (['vIZQARIT','vSUMARESTA']);   
     
     TAS[variables.vIZQARIT][terminales.tId] = new celda (['vRAIZPOT','vMULTDIV']);   
     TAS[variables.vIZQARIT][terminales.tParentesisAbre] = new celda (['vRAIZPOT','vMULTDIV']);  
     TAS[variables.vIZQARIT][terminales.tRaiz] = new celda (['vRAIZPOT','vMULTDIV']);   
-    TAS[variables.vIZQARIT][terminales.tMenos] = new celda (['vRAIZPOT','vMULTDIV']);   
+    TAS[variables.vIZQARIT][terminales.tResta] = new celda (['vRAIZPOT','vMULTDIV']);   
     TAS[variables.vIZQARIT][terminales.tConstReal] = new celda (['vRAIZPOT','vMULTDIV']);  
      
     TAS[variables.vRAIZPOT][terminales.tId] = new celda (['vOPERANDOS','vPOT']);   
     TAS[variables.vRAIZPOT][terminales.tParentesisAbre] = new celda (['vOPERANDOS','vPOT']);  
     TAS[variables.vRAIZPOT][terminales.tRaiz] = new celda (['tRaiz','tParentesisAbre','vEXPARIT','tParentesisCierra','vPOT']);
-    TAS[variables.vRAIZPOT][terminales.tMenos] = new celda (['vOPERANDOS','vPOT']);  
+    TAS[variables.vRAIZPOT][terminales.tResta] = new celda (['vOPERANDOS','vPOT']);  
     TAS[variables.vRAIZPOT][terminales.tConstReal] = new celda (['vOPERANDOS','vPOT']);   
 
     TAS[variables.vPOT][terminales.tProducto] = new celda (['tProducto','vOPERANDOS']); 
     
-    TAS[variables.vSUMARESTA][terminales.tMas] = new celda (['tMas','vOPERANDOS','vSUMARESTA']);
-    TAS[variables.vSUMARESTA][terminales.tMenos] = new celda (['tMenos','vOPERANDOS','vSUMARESTA']);   
+    TAS[variables.vSUMARESTA][terminales.tSuma] = new celda (['tSuma','vOPERANDOS','vSUMARESTA']);
+    TAS[variables.vSUMARESTA][terminales.tResta] = new celda (['tResta','vOPERANDOS','vSUMARESTA']);   
     
     TAS[variables.vMULTDIV][terminales.tProducto] = new celda (['tProducto','vOPERANDOS','vMULTDIV']);
     TAS[variables.vMULTDIV][terminales.tDivision] = new celda (['tDivision','vOPERANDOS','vMULTDIV']);   
 
     TAS[variables.vOPERANDOS][terminales.tId] = new celda (['tId']);
     TAS[variables.vOPERANDOS][terminales.tParentesisAbre] = new celda (['tParentesisAbre','vEXPARIT','tParentesisCierra']);
-    TAS[variables.vOPERANDOS][terminales.tMenos] = new celda (['tMenos','vOPERANDOS']);   
+    TAS[variables.vOPERANDOS][terminales.tResta] = new celda (['tResta','vOPERANDOS']);   
     TAS[variables.vOPERANDOS][terminales.tConstReal] = new celda (['tConstReal']);   
     
     TAS[variables.vLECTURA][terminales.tLeer] = new celda (['tLeer','tParentesisAbre','tCadena','tComa','tId']);   
@@ -165,7 +165,7 @@ export function cargarTAS(TAS:Array<any>):Array<any>{
     TAS[variables.vSALIDAS][terminales.tId] = new celda (['vSALIDA','vSAUX']);   
     TAS[variables.vSALIDAS][terminales.tParentesisAbre] = new celda (['vSALIDA','vSAUX']);   
     TAS[variables.vSALIDAS][terminales.tRaiz] = new celda (['vSALIDA','vSAUX']);   
-    TAS[variables.vSALIDAS][terminales.tMenos] = new celda (['vSALIDA','vSAUX']); 
+    TAS[variables.vSALIDAS][terminales.tResta] = new celda (['vSALIDA','vSAUX']); 
     TAS[variables.vSALIDAS][terminales.tConstReal] = new celda (['vSALIDA','vSAUX']);   
     TAS[variables.vSALIDAS][terminales.tCadena] = new celda (['vSALIDA','vSAUX']);   
 
@@ -174,7 +174,7 @@ export function cargarTAS(TAS:Array<any>):Array<any>{
     TAS[variables.vSALIDA][terminales.tId] = new celda (['vEXPARIT']);   
     TAS[variables.vSALIDA][terminales.tParentesisAbre] = new celda (['vEXPARIT']);   
     TAS[variables.vSALIDA][terminales.tRaiz] = new celda (['vEXPARIT']);   
-    TAS[variables.vSALIDA][terminales.tMenos] = new celda (['vEXPARIT']);   
+    TAS[variables.vSALIDA][terminales.tResta] = new celda (['vEXPARIT']);   
     TAS[variables.vSALIDA][terminales.tConstReal] = new celda (['vEXPARIT']);   
     TAS[variables.vSALIDA][terminales.tCadena] = new celda (['tCadena']);
     
@@ -187,7 +187,7 @@ export function cargarTAS(TAS:Array<any>):Array<any>{
     TAS[variables.vCONDICION][terminales.tId] = new celda (['vIZQCOND','vDISYUNCION']);   
     TAS[variables.vCONDICION][terminales.tParentesisAbre] = new celda (['vIZQCOND','vDISYUNCION']);   
     TAS[variables.vCONDICION][terminales.tRaiz] = new celda (['vIZQCOND','vDISYUNCION']);   
-    TAS[variables.vCONDICION][terminales.tMenos] = new celda (['vIZQCOND','vDISYUNCION']);   
+    TAS[variables.vCONDICION][terminales.tResta] = new celda (['vIZQCOND','vDISYUNCION']);   
     TAS[variables.vCONDICION][terminales.tConstReal] = new celda (['vIZQCOND','vDISYUNCION']);  
     TAS[variables.vCONDICION][terminales.tCorcheteAbre] = new celda (['vIZQCOND','vDISYUNCION']);   
     TAS[variables.vCONDICION][terminales.tNot] = new celda (['vIZQCOND','vDISYUNCION']);   
@@ -195,7 +195,7 @@ export function cargarTAS(TAS:Array<any>):Array<any>{
     TAS[variables.vIZQCOND][terminales.tId] = new celda (['vNEGACION','vCONJUNCION']);   
     TAS[variables.vIZQCOND][terminales.tParentesisAbre] = new celda (['vNEGACION','vCONJUNCION']);   
     TAS[variables.vIZQCOND][terminales.tRaiz] = new celda (['vNEGACION','vCONJUNCION']);   
-    TAS[variables.vIZQCOND][terminales.tMenos] = new celda (['vNEGACION','vCONJUNCION']);   
+    TAS[variables.vIZQCOND][terminales.tResta] = new celda (['vNEGACION','vCONJUNCION']);   
     TAS[variables.vIZQCOND][terminales.tConstReal] = new celda (['vNEGACION','vCONJUNCION']);  
     TAS[variables.vIZQCOND][terminales.tCorcheteAbre] = new celda (['vNEGACION','vCONJUNCION']);   
     TAS[variables.vIZQCOND][terminales.tNot] = new celda (['vNEGACION','vCONJUNCION']);   
@@ -203,7 +203,7 @@ export function cargarTAS(TAS:Array<any>):Array<any>{
     TAS[variables.vNEGACION][terminales.tId] = new celda (['vEXPARIT','tOpRel','vEXPARIT']);   
     TAS[variables.vNEGACION][terminales.tParentesisAbre] = new celda (['vEXPARIT','tOpRel','vEXPARIT']);   
     TAS[variables.vNEGACION][terminales.tRaiz] = new celda (['vEXPARIT','tOpRel','vEXPARIT']);   
-    TAS[variables.vNEGACION][terminales.tMenos] = new celda (['vEXPARIT','tOpRel','vEXPARIT']);   
+    TAS[variables.vNEGACION][terminales.tResta] = new celda (['vEXPARIT','tOpRel','vEXPARIT']);   
     TAS[variables.vNEGACION][terminales.tConstReal] = new celda (['vEXPARIT','tOpRel','vEXPARIT']);   
     TAS[variables.vNEGACION][terminales.tCorcheteAbre] = new celda (['tCorcheteAbre','vCONDICION','tCorcheteCierra']); 
     TAS[variables.vNEGACION][terminales.tNot] = new celda (['tNot','vNEGACION']);   
