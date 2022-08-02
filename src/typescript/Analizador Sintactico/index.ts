4ba1de9327b5a575869b643a574431a8058c3df7
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-08-02 18:13:58 -0300

    Cambios menores

--- src/typescript/Analizador Sintactico/index.ts
+++ src/typescript/Analizador Sintactico/index.ts
@@ -20,7 +20,6 @@ import { evaluarPrograma } from "../Analizador Semantico/index.js";
 // import { evaluarPrograma } from "../Analizador Semantico/index.ts"; 
 
 
-
 const arrayVariables = ['vPROGRAMA','vCUERPO','vSENTENCIAS','vSENTENCIA','vDECLARACION','vVARIABLES','vVARIABLE','vASIGNACION','vEXPARIT','vIZQARIT','vRAIZPOT','vPOT','vSUMARESTA','vMULTDIV','vOPERANDOS','vLECTURA','vESCRITURA','vSALIDAS','vSAUX','vSALIDA','vCONDICIONAL','vCONDICIONALFACT','vMIENTRAS','vCONDICION','vIZQCOND','vNEGACION','vCONJUNCION', 'vDISYUNCION']
   
 const arrayTerminales = ['tPrograma' , 'tEscribir' , 'tVariables' , 'tLeer' , 'tWhile' , 'tIf' , 'tElse' , 'tAnd' , 'tOr' , 'tNot' , 
@@ -46,7 +45,7 @@ export function analisisSintactico(codigoFuente:string , raiz:Arbol):void{
     let errorLog:string = '';
     let control: number = 0;
     let lexema:string ='';
-    let compLex: simboloGramatical; // tiene que ser de tipo simboloGramatical
+    let compLex: simboloGramatical;
     let TAS = creaTAS();
     let pila: Array<elementoPila> = crearPila();
     let tablaSimbolos = TS; // Igual a la que importamos (esto es para poder ir actualizandola)
@@ -58,16 +57,11 @@ export function analisisSintactico(codigoFuente:string , raiz:Arbol):void{
     tablaSimbolos = nodoCompLex[3];
     control = nodoCompLex[1]
 
-    // console.log('compLex encontrado: ' + compLex)
-    // console.log('lexema encontrado: ' + lexema)
-    // console.log('tabla de simbolos ' + JSON.stringify(tablaSimbolos))
-
     let x: elementoPila;
     while(compLex !== 'errorLexico' && !exito){
         x = Desapilar(pila) as any // Obtengo un elemento de la pila de elementos a encontrar en el programa.
         
         if (arrayTerminales.includes(x.simbolo)){
-            // console.log(x.simbolo + ' es terminal')
             if (x.simbolo == compLex){
                 if (x.arbolPila){
                     x.arbolPila.lexema = lexema // Si es distinto de undefined, lo asigna (esta comprobacion es por ts)
@@ -80,7 +74,6 @@ export function analisisSintactico(codigoFuente:string , raiz:Arbol):void{
                 }
                 lexema = nodoCompLex[2];
                 control = nodoCompLex[1]
-                // console.log('Se obtuvo el elemento: ' + compLex)
             } else {
                 errorLog = ' se esperaba ' + x.simbolo + ' y se encontro ' + compLex;
                 compLex = 'errorLexico'; // No es un error lexico pero sirve para cortar el while.
@@ -88,12 +81,10 @@ export function analisisSintactico(codigoFuente:string , raiz:Arbol):void{
         }
 
         else if (arrayVariables.includes(x.simbolo)){
-        // console.log(x.simbolo + ' es una variable');
         let posicion1 = variables[x.simbolo as any]
         let posicion2 = terminales[compLex as any]
             if (TAS[posicion1 as any][posicion2 as any] === undefined){
                 errorLog = '<< TAS no definida para '+ x.simbolo + ' hacia ' + compLex + ' >>';
-                // console.log('TAS NO DEFINIDA PARA '+ x.simbolo + ' HACIA ' + compLex)
                 compLex = 'errorLexico'; // No es un error lexico pero sirve para cortar el while.
             } else{
                 let contador:number= 0;
@@ -103,7 +94,6 @@ export function analisisSintactico(codigoFuente:string , raiz:Arbol):void{
                     let compL : simboloGramatical = TAS[posicion1 as any][posicion2 as any].elementos[contador]; // Guardo el elemento del array que contiene todos los elementos d la parte derecha.
                     let hijo = new nodo(compL,'',0,[]);
                     x.arbolPila?.insertarHijo(hijo);
-                    // console.log('Nuevo nodo en el arbol ' + compL)
                     contador++
                 }
 
@@ -124,7 +114,6 @@ export function analisisSintactico(codigoFuente:string , raiz:Arbol):void{
         }
     } 
     if (exito){
-        // console.log('****** Sintaxis correcta ******');
         mostrarInfoSintactico([true],raiz)
         evaluarPrograma(raiz);
     } else {
