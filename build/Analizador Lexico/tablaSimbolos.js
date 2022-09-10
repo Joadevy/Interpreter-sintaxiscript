61f90973b5e8ba5c4e15d35252ef797ab0a90e3b
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-09-10 02:06:43 -0300

    code minimized

--- build/Analizador Lexico/tablaSimbolos.js
+++ build/Analizador Lexico/tablaSimbolos.js
@@ -1,14 +1,1 @@
-export let tablaSimbolos = {
-    // Tiene la forma de lexema: componente lexico, se ira actualizando a medida que se encuentren identificadores.
-    'PROGRAM': 'tPrograma',
-    'PRINT': 'tEscribir',
-    'VAR': 'tVariables',
-    'READ': 'tLeer',
-    'WHILE': 'tWhile',
-    'IF': 'tIf',
-    'ELSE': 'tElse',
-    'AND': 'tAnd',
-    'OR': 'tOr',
-    'NOT': 'tNot',
-    'RAIZ': 'tRaiz'
-};
+export let tablaSimbolos={PROGRAM:"tPrograma",PRINT:"tEscribir",VAR:"tVariables",READ:"tLeer",WHILE:"tWhile",IF:"tIf",ELSE:"tElse",AND:"tAnd",OR:"tOr",NOT:"tNot",RAIZ:"tRaiz"};
